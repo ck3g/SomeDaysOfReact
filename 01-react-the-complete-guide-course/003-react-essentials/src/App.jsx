c315f525e317172fa85b1778bea0cf308175b607
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-02-28 08:48:23 +0100

    Display content conditionally

--- 01-react-the-complete-guide-course/003-react-essentials/src/App.jsx
+++ 01-react-the-complete-guide-course/003-react-essentials/src/App.jsx
@@ -8,10 +8,22 @@ import TabButton from './components/TabButton.jsx';
 function App() {
   // useState should be called inside components and not nested into some other if conditions etc.
   // it always returns an array of two elements
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+      </div>
+    )
   }
 
   return (
@@ -39,11 +51,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
