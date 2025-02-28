f3b0e89f534b43f3f72d35152aa0233780740bb8
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-02-28 08:37:43 +0100

    Use proper data for examples

--- 01-react-the-complete-guide-course/003-react-essentials/src/App.jsx
+++ 01-react-the-complete-guide-course/003-react-essentials/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
@@ -8,7 +8,7 @@ import TabButton from './components/TabButton.jsx';
 function App() {
   // useState should be called inside components and not nested into some other if conditions etc.
   // it always returns an array of two elements
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -39,7 +39,11 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
+          </div>
         </section>
       </main>
     </div>
