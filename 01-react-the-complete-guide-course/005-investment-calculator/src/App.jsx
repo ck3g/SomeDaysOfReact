4eef9e7eff428ad1c683431afd0449e8414adb70
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-11 20:08:10 +0100

    Add a Results component and pass user input to it

--- 01-react-the-complete-guide-course/005-investment-calculator/src/App.jsx
+++ 01-react-the-complete-guide-course/005-investment-calculator/src/App.jsx
@@ -1,11 +1,30 @@
 import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  function handleChange(inputID, newValue) {
+    setUserInput(wasInput => {
+      return {
+        ...wasInput,
+        [inputID]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   )
 }
