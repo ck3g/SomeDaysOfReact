57ac798aea8d1bdaae301c1ed7f712ec5dfc5f7c
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-11 18:07:24 +0100

    Render Game Over popup

--- 01-react-the-complete-guide-course/004-tic-tac-toe/src/App.jsx
+++ 01-react-the-complete-guide-course/004-tic-tac-toe/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
+import GameOver from './components/GameOver.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
 const initialGameBoard = [
@@ -50,6 +51,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length == 9 && !winner;
+
   function handleSelectSquare(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
       const activePlayer = deriveActivePlayer(prevTurns);
@@ -74,7 +77,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
 
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
 
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
