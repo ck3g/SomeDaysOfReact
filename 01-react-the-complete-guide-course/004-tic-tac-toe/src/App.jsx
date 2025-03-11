c10503a97623a3a30fb76e1053ab1101cd297186
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-11 18:39:22 +0100

    Clean up App component

--- 01-react-the-complete-guide-course/004-tic-tac-toe/src/App.jsx
+++ 01-react-the-complete-guide-course/004-tic-tac-toe/src/App.jsx
@@ -6,7 +6,13 @@ import Log from './components/Log.jsx';
 import GameOver from './components/GameOver.jsx';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
 
-const initialGameBoard = [
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+};
+
+const INITIAL_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -21,24 +27,7 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [players, setPlayers] = useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  })
-  const [gameTurns, setGameTurns] = useState([]);
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  let gameBoard = [...initialGameBoard].map(arr => [...arr]);
-
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+function deriveWinner(gameBoard, players) {
   let winner = null;
 
   for (const combination of WINNING_COMBINATIONS) {
@@ -55,6 +44,29 @@ function App() {
     }
   }
 
+  return winner
+}
+
+function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_BOARD].map(arr => [...arr]);
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS)
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns)
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length == 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -91,13 +103,13 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === 'X'}
             onChangeName={handlePlayerNameChage}
           />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === 'O'}
             onChangeName={handlePlayerNameChage}
