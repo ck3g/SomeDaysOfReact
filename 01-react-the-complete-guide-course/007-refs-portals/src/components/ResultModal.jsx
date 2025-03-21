b4840fc29147784b26b943a61d1d88fd7a2a0fa0
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-21 10:56:43 +0100

    Display you lost or remaining time

--- 01-react-the-complete-guide-course/007-refs-portals/src/components/ResultModal.jsx
+++ 01-react-the-complete-guide-course/007-refs-portals/src/components/ResultModal.jsx
@@ -2,8 +2,11 @@ import { useImperativeHandle, useRef } from 'react';
 
 // For older version of React passing `ref` as prop won't work
 // We need to use `forwardRef` instead
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,10 +17,10 @@ export default function ResultModal({ ref, result, targetTime }) {
   });
 
   return <dialog ref={dialog} className="result-modal">
-    <h2>You {result}</h2>
+    {userLost && <h2>You lost</h2>}
     <p>The target time was <strong>{targetTime} seconds</strong>.</p>
-    <p>You stopped the timer with <strong>X seconds left.</strong></p>
-    <form method="dialog">
+    <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+    <form method="dialog" onSubmit={onReset}>
       <button>Close</button>
     </form>
   </dialog>
--- 01-react-the-complete-guide-course/007-refs-portals/src/components/TimerChallenge.jsx
+++ 01-react-the-complete-guide-course/007-refs-portals/src/components/TimerChallenge.jsx
@@ -12,7 +12,6 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000)
     dialog.current.open();
   }
 
@@ -27,9 +26,19 @@ export default function TimerChallenge({ title, targetTime }) {
     clearInterval(timer.current);
   }
 
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        result="lost"
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
