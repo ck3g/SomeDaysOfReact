43f952a37c60f29eebf8fbcbb92c1053c97e04d7
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-21 15:48:41 +0100

    Create a ProjectsSidebar controller

---
+++ 01-react-the-complete-guide-course/008-project-management/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,14 @@
+export default function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your projects</h2>
+      <div>
+        <button>
+          + Add project
+        </button>
+      </div>
+      <ul>
+      </ul>
+    </aside>
+  );
+}
