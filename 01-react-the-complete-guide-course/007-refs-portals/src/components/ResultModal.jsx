defe410186ca87693af0fb58b0cf383fff1e21fd
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-21 11:09:27 +0100

    use createPortal to display modals at the top of the DOM

--- 01-react-the-complete-guide-course/007-refs-portals/src/components/ResultModal.jsx
+++ 01-react-the-complete-guide-course/007-refs-portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 // For older version of React passing `ref` as prop won't work
 // We need to use `forwardRef` instead
@@ -17,13 +18,16 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
     }
   });
 
-  return <dialog ref={dialog} className="result-modal" onClose={onReset}>
-    {userLost && <h2>You lost</h2>}
-    {!userLost && <h2>Your score: {score}</h2>}
-    <p>The target time was <strong>{targetTime} seconds</strong>.</p>
-    <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-    <form method="dialog" onSubmit={onReset}>
-      <button>Close</button>
-    </form>
-  </dialog>
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+      <p>The target time was <strong>{targetTime} seconds</strong>.</p>
+      <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal')
+  )
 }
