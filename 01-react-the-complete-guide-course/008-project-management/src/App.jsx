02a86e5fbe8cfd705a347c26b161d5974ec7a867
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-21 16:26:34 +0100

    Allow to toggle NewProject component

--- 01-react-the-complete-guide-course/008-project-management/src/App.jsx
+++ 01-react-the-complete-guide-course/008-project-management/src/App.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react';
+
 import ProjectsSidebar from './components/ProjectsSidebar.jsx';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    // undefined - show no projects selected
+    // null - new project
+    // <id> - selected project
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
