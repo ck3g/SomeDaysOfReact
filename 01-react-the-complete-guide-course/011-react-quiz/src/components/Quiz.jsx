3de8f10f36fa81297b5a25113955d0ffffbf29ee
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-23 15:36:26 +0100

    Add Quiz component with the list of questions and answers

---
+++ 01-react-the-complete-guide-course/011-react-quiz/src/components/Quiz.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import QUESTIONS from '../questions.js';
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  // because we want to use as little state as possible
+  // and derive as much state as possible
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  }
+
+  return <div id="quiz">
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+}
