cf91c34f33593ac9c2cb96dad22bdf8c9a8b246c
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-23 16:26:04 +0100

    Add QuestionTimer component with a progressbar

---
+++ 01-react-the-complete-guide-course/011-react-quiz/src/components/QuestionTimer.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+export default function QuestionTimer({ timeout, onTimeout }) {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [onTimeout])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime(prevTime => prevTime - 100)
+    }, 100)
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
+
+  return <progress id="question-time" value={remainingTime} max={timeout} />;
+}
