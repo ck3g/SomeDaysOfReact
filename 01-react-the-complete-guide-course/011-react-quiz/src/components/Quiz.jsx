f745bb6b63e900d31d080ceaff79a41641b09a3f
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-03-23 16:33:21 +0100

    Wrap handle (skip) answer into useCallback

--- 01-react-the-complete-guide-course/011-react-quiz/src/components/Quiz.jsx
+++ 01-react-the-complete-guide-course/011-react-quiz/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 import quizComplete from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
@@ -12,11 +12,16 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
-    setUserAnswers((prevAnswers) => {
-      return [...prevAnswers, selectedAnswer];
-    });
-  }
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setUserAnswers((prevAnswers) => {
+        return [...prevAnswers, selectedAnswer];
+      });
+    },
+    []
+  )
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
 
   if (quizIsComplete) {
     return <div id="summary">
@@ -30,7 +35,7 @@ export default function Quiz() {
 
   return <div id="quiz">
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />
+      <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
       <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
       <ul id="answers">
         {shuffledAnswers.map((answer) => (
