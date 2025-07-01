d114709632afcf290ee0a32ca031a4991bd4eb8a
Vitali Tatarintev <vtatarintev@gitlab.com>
2025-07-01 17:33:38 +0200

    Validate on submit

--- 01-react-the-complete-guide-course/016-react-forms/src/components/Login.jsx
+++ 01-react-the-complete-guide-course/016-react-forms/src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState();
+
   const email = useRef();
   const password = useRef();
 
@@ -8,10 +10,21 @@ export default function Login() {
     event.preventDefault();
     console.log('User email ' + email.current.value);
     console.log('User password ' + password.current.value);
+
+    const emailIsValid = email.current.value.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log('Sending HTTP request...');
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} noValidate>
       <h2>Login</h2>
 
       <div className="control-row">
@@ -23,6 +36,7 @@ export default function Login() {
             name="email"
             ref={email}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
